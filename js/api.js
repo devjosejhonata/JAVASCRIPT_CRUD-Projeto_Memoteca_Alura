36ef9056984713df6fb4957082fc1d3be6c89c44
Jose Jhonata <contatojosejhonata@gmail.com>
2024-10-03 16:10:18 -0300

    Criando a requisição api com o metodo POST para adicionar um novo dado

--- js/api.js
+++ js/api.js
@@ -11,6 +11,23 @@ const api = {
 
             throw error
         }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/json"
+                }, 
+                body: JSON.stringify(pensamento)
+            });
+            return await response.json();
+        } catch (error) {
+            alert('Erro ao cadastrar pensamento')
+
+            throw error
+        }
     }
 }
 
