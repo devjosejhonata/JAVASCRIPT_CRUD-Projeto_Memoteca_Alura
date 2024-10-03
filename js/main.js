9307d19970e8e649b3f07bc2905c2a7bc7e474d5
Jose Jhonata <contatojosejhonata@gmail.com>
2024-10-03 16:46:00 -0300

    Implementando a lógica de adicionar novo pensamento na interface e refatorando o código

--- js/main.js
+++ js/main.js
@@ -3,5 +3,26 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos();
+
+    const formularioPensamento = document.getElementById("pensamento-form");
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario);
+
 });
+
+//cadastrar novo pensamento
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+
+    const id = document.getElementById("pensamento-id").value;
+    const conteudo = document.getElementById("pensamento-conteudo").value;
+    const autoria = document.getElementById("pensamento-autoria").value;
+
+    try {
+        await api.salvarPensamento({ conteudo, autoria });
+        ui.renderizarPensamentos();
+    } catch {
+        alert("Erro ao salvar pensamento");
+    }
+
+}
 
