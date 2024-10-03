d75aaa8ce9422e903aa8c22ee8ebf9f2539aee1f
Jose Jhonata <contatojosejhonata@gmail.com>
2024-10-03 17:51:55 -0300

    Codificando para editar dados de um pensamento escolhido e enviar novamente para o servidor

--- js/api.js
+++ js/api.js
@@ -2,6 +2,8 @@
 /* Arquivo responsável por conter as requisições com a api */
 
 const api = {
+
+    //buscando dados
     async buscarPensamentos() {
         try {
             const response = await fetch('http://localhost:3000/pensamentos');
@@ -12,7 +14,8 @@ const api = {
             throw error
         }
     },
-
+    
+    //adicionando novo pensamento
     async salvarPensamento(pensamento) {
         try {
             const response = await fetch('http://localhost:3000/pensamentos', {
@@ -24,11 +27,41 @@ const api = {
             });
             return await response.json();
         } catch (error) {
-            alert('Erro ao cadastrar pensamento')
+            alert('Erro ao salvar pensamento')
 
             throw error
         }
-    }
+    }, 
+    
+    //buscando pensamento escolhido por id
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+            return await response.json();
+        } catch (error) {
+            alert('Erro ao buscar pensamentos')
+
+            throw error
+        }
+    },
+    
+    //editando pensamento escolhido
+    async editarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type" : "application/json"
+                }, 
+                body: JSON.stringify(pensamento)
+            });
+            return await response.json();
+        } catch (error) {
+            alert('Erro ao editar pensamento')
+
+            throw error
+        }
+    }, 
 }
 
 
